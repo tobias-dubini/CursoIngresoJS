0efe897f589e11a9eba6d43427a773b84c8ad6d5
tobias dubini <you@example.com>
2021-07-06 11:58:29 -0300

    ejercicios de la primer clase

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,9 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	// declaracion de una variable 
+	let nombre;
+	nombre=prompt("Ingrese su nombre:")   /*promp, caja emergente*/
+	alert(nombre);
 }
 
